96c6d58da09da163e6fb75e55c11babbaecd9e36
Stefan <shariton@pycoding.biz>
2015-09-21 20:33:58 +0300

    Removed console logs from post deploy hook

--- src/DynamicImage/hook.post-deploy.js
+++ src/DynamicImage/hook.post-deploy.js
@@ -12,20 +12,17 @@ var async = require('async');
 var exports = module.exports = function(callback) {
   var bucketName = parameters.backend.s3Bucket;
 
-  console.log(bucketName);
   async.parallel([
     updateBucketWebsite,
     put1x1EmptyPixel,
   ], callback);
 
   function updateBucketWebsite(cb) {
-    console.log('updating bucket website');
     var apiGatewayHostname = parameters.backend.apiGatewayHostname;
     var apiGatewayPrefix = parameters.backend.apiGatewayPrefix;
 
     s3.getBucketWebsite({Bucket: bucketName}, function(err, existingWebsiteConfiguration) {
       if (err) {
-        console.log(err);
         cb(err);
       };
 
@@ -60,12 +57,9 @@ var exports = module.exports = function(callback) {
       };
 
       s3.putBucketWebsite(websiteConfiguration, function(err, response) {
-        console.log('put new configuration');
         if (err) {
-          console.log(err);
           cb(err);
         } else {
-          console.log(response);
           cb(response);
         }
       });
@@ -73,7 +67,6 @@ var exports = module.exports = function(callback) {
   }
 
   function put1x1EmptyPixel(cb) {
-    console.log('putting 1x1 pixel');
     var base64EmptyPixel = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGNiYAAAAAkAAxkR2eQAAAAASUVORK5CYII=';
     var parameters = {
       Bucket: bucketName,
@@ -82,10 +75,8 @@ var exports = module.exports = function(callback) {
 
     s3.getObject(parameters, function(err, response) {
       if (err) {
-        console.log('pixel not found');
         parameters.Body = new Buffer(base64EmptyPixel, 'base64');
         s3.putObject(parameters, function(err, response) {
-          console.log('pixel put');
           if (err) {
             cb(err);
           } else {
