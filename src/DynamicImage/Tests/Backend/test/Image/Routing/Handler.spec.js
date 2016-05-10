b969a0b6f416df966a22ed824f32776f996881a7
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-10 12:25:37 +0300

    Encrypt slack token

---
+++ src/DynamicImage/Tests/Backend/test/Image/Routing/Handler.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue May 10 2016 12:13:41 GMT+0300 (EEST)
+
+'use strict';
+
+import chai from 'chai';
+import Handler from '../../../node_modules/Image/Routing/Handler';
+
+// @todo: Add more advanced tests
+suite('Handlers', () => {
+  test('Class Handler exists in deep-microservices-dynamic-image-routing module', () => {
+    chai.expect(Handler).to.be.an('function');
+  });
+});
